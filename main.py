a3723bfc178ce1758b91feb5c8ab24a4f6973ee2
mechabubba <stevenvanniisprettycool@gmail.com>
2025-11-20 14:31:51 -0600

    add sql connection + readme instructions

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
 import configparser
 import os
+import sys
 
-config_path = os.path.join(os.path.dirname(__file__), "file", "config.ini")
+from flask import Flask, render_template
+import mysql.connector
+
+# step one: find ya config
+config_path = os.path.join(os.path.dirname(__file__), "config.ini")
 config = configparser.ConfigParser()
 config.read(config_path)
+db_cfg = config["db"]
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+# step two: log in
+try:
+    conn = mysql.connector.connect(
+        host=db_cfg["host"],
+        user=db_cfg["user"],
+        password=db_cfg["password"],
+        database=db_cfg["database"]
+    )
+except mysql.connector.Error as e:
+    print("Database connection failed!")
+    print(e)
+    sys.exit(1)
+
+# step three: ignition
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def home():
